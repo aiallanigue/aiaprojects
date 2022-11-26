6d9443cd7fab12948b8b6d578416bb4f753fb1a2
Lan Allanigue <allanallanigue@gmail.com>
2022-11-27 05:16:20 +0800

    Landing Page

---
+++ v3_aiablog/src/components/FooterView.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-footer>
+      <v-card
+        elevation="0"
+        rounded="0"
+        width="100%"
+        class="bg-grey text-center"
+      >
+        <v-card-text>
+          <v-btn
+            class="mx-4"
+            icon="fa-brands fa-facebook"
+            variant="plain"
+            color="white"
+            href="https://www.facebook.com/a11anigue/"
+            target="_blank"
+          ></v-btn>
+          <v-btn
+            class="mx-4"
+            icon="fa-brands fa-instagram"
+            variant="plain"
+            color="white"
+            href="https://www.instagram.com/aia11anigue/"
+            target="_blank"
+          ></v-btn>
+        </v-card-text>
+
+        <v-card-text class="text-white">
+          @ {{ new Date().getFullYear() }} —
+          <strong>AIAllanigue Blog</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.v-footer {
+  padding: 8px 0 !important;
+}
+</style>
---
+++ v3_aiablog/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app-bar app color="black" dark flat class="px-12" >
+      <v-btn >
+        <v-icon color="red" left class="mr-2">fas fa-signature</v-icon> CAR CUSTOMS
+      </v-btn>
+      
+  
+      <v-spacer></v-spacer>
+      <v-btn text @click="scroll('home')" class="text-red">Home</v-btn>
+      <v-btn text @click="scroll('about')"  >About</v-btn>
+      <v-btn text @click="scroll('portfolio')"  >Portfolio</v-btn>
+      <v-btn text @click="scroll('services')" > Services</v-btn>
+      <v-btn text @click="scroll('page')"  > Page</v-btn>
+      <v-btn text @click="scroll('biog')"  > Blog</v-btn>
+      <v-btn text @click="scroll('contact')"  > Contact</v-btn>
+    </v-app-bar>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" });
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
